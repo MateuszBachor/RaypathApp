891b5120ad59c2395fceeb2789f611259a468d2c
MateuszBachor <mateuszbachor3@gmail.com>
2026-05-10 11:33:39 +0200

    Center navBar items

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     }`}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 container">
         <div className="flex justify-between items-center">
-          <div className="flex items-center center-items">
+          <div className="flex items-center ">
             <span className="text-2xl font-bold tracking-tighter">
               <span className="text-green-700">ECO</span>
               <span className="text-blue-600">PARTNER</span>
